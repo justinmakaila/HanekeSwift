2a6a5cd5d8307ac1aa4a37f88b5f94df60dfebf6
Justin Makaila <justinmakaila@gmail.com>
2015-03-27 16:43:45 -0400

    Made properties and methods public...
    Public classes with no public properties or methods are useless.

--- Haneke/Fetcher.swift
+++ Haneke/Fetcher.swift
@@ -11,31 +11,31 @@ import UIKit
 // See: http://stackoverflow.com/questions/25915306/generic-closure-in-protocol
 public class Fetcher<T : DataConvertible> {
 
-    let key : String
+    public let key : String
     
     init(key : String) {
         self.key = key
     }
     
-    func fetch(failure fail : ((NSError?) -> ()), success succeed : (T.Result) -> ()) {}
+    public func fetch(failure fail : ((NSError?) -> ()), success succeed : (T.Result) -> ()) {}
     
-    func cancelFetch() {}
+    public func cancelFetch() {}
 }
 
-class SimpleFetcher<T : DataConvertible> : Fetcher<T> {
+public class SimpleFetcher<T : DataConvertible> : Fetcher<T> {
     
     let getValue : () -> T.Result
     
-    init(key : String, value getValue : @autoclosure () -> T.Result) {
+    public init(key : String, value getValue : @autoclosure () -> T.Result) {
         self.getValue = getValue
         super.init(key: key)
     }
     
-    override func fetch(failure fail : ((NSError?) -> ()), success succeed : (T.Result) -> ()) {
+    override public  func fetch(failure fail : ((NSError?) -> ()), success succeed : (T.Result) -> ()) {
         let value = getValue()
         succeed(value)
     }
     
-    override func cancelFetch() {}
+    override public func cancelFetch() {}
     
 }
